256424b32f0a39a0d1191c84cc2a7ba77a3ce54d
guoyecn <128583878+guoyecn@users.noreply.github.com>
2023-06-15 16:07:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,28 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.16.1/g' package/base-files/files/bin/config_generate
-cat >> target/linux/ipq807x/image/generic.mk <<EOF
-
-define Device/redmi_ax6
-	$(call Device/xiaomi_ax3600)
-	DEVICE_VENDOR := Redmi
-	DEVICE_MODEL := AX6
-	DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
-endef
-TARGET_DEVICES += redmi_ax6
-
-define Device/xiaomi_ax3600
-	$(call Device/FitImage)
-	$(call Device/UbiFit)
-	DEVICE_VENDOR := Xiaomi
-	DEVICE_MODEL := AX3600
-	BLOCKSIZE := 128k
-	PAGESIZE := 2048
-	DEVICE_DTS_CONFIG := config@ac04
-	SOC := ipq8071
-	DEVICE_PACKAGES := ath10k-firmware-qca9887-ct ipq-wifi-xiaomi_ax3600 \
-	kmod-ath10k-ct uboot-envtools
-endef
-TARGET_DEVICES += xiaomi_ax3600
-
-EOF
+cp -f $GITHUB_WORKSPACE/ax6.mk openwrt/target/linux/ipq807x/image/generic.mk
