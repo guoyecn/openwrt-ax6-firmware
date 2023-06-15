930855b92dad338e48e11930dd113390e86dbaa5
guoyecn <128583878+guoyecn@users.noreply.github.com>
2023-06-15 14:59:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,9 +17,9 @@ cat >> target/linux/ipq807x/image/generic.mk <<EOF
 define Device/redmi_ax6
         $(call Device/xiaomi_ax3600)
         DEVICE_VENDOR := Redmi
-        DEVICE_MODEL := AX6
+        DEVICE_MODEL := AX6  
         DEVICE_PACKAGES := ipq-wifi-redmi_ax6 uboot-envtools
-endef
+endef  
 TARGET_DEVICES += redmi_ax6
 
 EOF
